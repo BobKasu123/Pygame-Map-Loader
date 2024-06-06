de31af50af106200b407ceeb2cd675e1b0a74d40
BobKasu123 <167287325+BobKasu123@users.noreply.github.com>
2024-06-06 20:19:36 +0400

    Added txt file map loading and list map loading

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import pygame
 
 def load_file_map(screen, file: str, images: dict, x: int, y: int):
-  file.split("\n")
+  map = file.split("\n")
   x = x
   y = y
-  for row in file:
-    for tile, img_num in zip(row, images):
-      if tile == img_num:
-        screen.blit(pygame.image.load(images[img_num]), (x, y)
-      x += 5
-    y += 5
-
-def load_map(screen, map: list, images: dict):
-  x = x
-  y = y
-  if len(map) > 0:
+  if len(file) > 0:
     for row in map:
       for tile, img_num in zip(row, images):
-        if num == img_num:
-          screen.blit(pygame.image.load(images[img_num]), (x, y))
+        if tile == img_num:
+          screen.blit(pygame.image.load(images[img_num]), (x, y)
         else:
           # automatically skips numbers that are not defined in the images dict
           continue
@@ -31,8 +21,22 @@ def load_map(screen, map: list, images: dict):
       else:
         continue
         
-        
-                    
-
-
-                  
+def load_map(screen, map: list, images: dict):
+  x = x
+  y = y
+  if len(map) > 0:
+    for row in map:
+      for tile, img_num in zip(row, images):
+        if num == img_num:
+          screen.blit(pygame.image.load(images[img_num]), (x, y))
+        else:
+          continue
+        x += 5
+      y += 5
+  else:
+    for tile, img_num in zip(map, images):
+      if tile == img_num:
+        screen.blit(pygame.image.load(images[img_num]), (x, y))
+      else:
+        continue
+                                         
