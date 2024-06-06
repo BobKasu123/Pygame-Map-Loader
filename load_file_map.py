17c8284828f2d2904187c3d1e85156c4695675ad
BobKasu123 <167287325+BobKasu123@users.noreply.github.com>
2024-06-06 20:15:25 +0400

    Added file map loading and list map loading

---
+++ load_file_map.py
@@ -0,0 +1,38 @@
+import pygame
+
+def load_file_map(screen, file: str, images: dict, x: int, y: int):
+  file.split("\n")
+  x = x
+  y = y
+  for row in file:
+    for tile, img_num in zip(row, images):
+      if tile == img_num:
+        screen.blit(pygame.image.load(images[img_num]), (x, y)
+      x += 5
+    y += 5
+
+def load_map(screen, map: list, images: dict):
+  x = x
+  y = y
+  if len(map) > 0:
+    for row in map:
+      for tile, img_num in zip(row, images):
+        if num == img_num:
+          screen.blit(pygame.image.load(images[img_num]), (x, y))
+        else:
+          # automatically skips numbers that are not defined in the images dict
+          continue
+        x += 5
+      y += 5
+  else:
+    for tile, img_num in zip(map, images):
+      if tile == img_num:
+        screen.blit(pygame.image.load(images[img_num]), (x, y))
+      else:
+        continue
+        
+        
+                    
+
+
+                  
