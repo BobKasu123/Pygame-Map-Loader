99e53184d3c09e8bcdcc54ef85eb485c9971ea2d
BobKasu123 <167287325+BobKasu123@users.noreply.github.com>
2024-06-19 20:14:42 +0400

    Added file map loading and list map loading

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import pygame
 
+# Fixed some stuff
+
 Class MapLoader:
-  def load_file_map(screen, file: str, images: dict, x: int, y: int):
+  def load_file_map(screen, file: str, images: dict, x: int, y: int, width: int, height: int):
     map = file.split("\n")
     if len(file) > 0:
       for row in map:
         for tile, img_num in zip(row, images):
           if tile == img_num:
-            screen.blit(pygame.image.load(images[img_num]), (x, y)
-          x += 5
-        y += 5
+            screen.blit(pygame.transform.scale(pygame.image.load(images[img_num]), (width, height)), (x, y))
+          x += width
+        y += height
     else:
       for tile, img_num in zip(map, images):
         if tile == img_num:
-          screen.blit(pygame.image.load(images[img_num]), (x, y))
+          screen.blit(pygame.transform.scale(pygame.image.load(images[img_num]), (width, height)), (x, y))
           
   def load_map(screen, map: list, images: dict, x, y):
     if len(map) > 0:
       for row in map:
         for tile, img_num in zip(row, images):
           if num == img_num:
-            screen.blit(pygame.image.load(images[img_num]), (x, y))
-          x += 5
-        y += 5
+            screen.blit(pygame.transform.scale(pygame.image.load(images[img_num]), (width, height)), (x, y))
+          x += width
+        y += height
     else:
       for tile, img_num in zip(map, images):
         if tile == img_num:
-          screen.blit(pygame.image.load(images[img_num]), (x, y))
+          screen.blit(pygame.transform.scale(pygame.image.load(images[img_num]), (width, height)), (x, y))
