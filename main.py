ac71b12d113bce50472b4013eb559c194977611d
BobKasu123 <167287325+BobKasu123@users.noreply.github.com>
2024-06-19 20:09:56 +0400

    Added file map loading and list map loading

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ import pygame
 Class MapLoader:
   def load_file_map(screen, file: str, images: dict, x: int, y: int):
     map = file.split("\n")
-    x = x
-    y = y
     if len(file) > 0:
       for row in map:
         for tile, img_num in zip(row, images):
           if tile == img_num:
             screen.blit(pygame.image.load(images[img_num]), (x, y)
-          else:
-            # automatically skips numbers that are not defined in the images dict
-            continue
           x += 5
         y += 5
     else:
@@ -22,16 +17,12 @@ Class MapLoader:
         else:
           continue
           
-  def load_map(screen, map: list, images: dict):
-    x = x
-    y = y
+  def load_map(screen, map: list, images: dict, x, y):
     if len(map) > 0:
       for row in map:
         for tile, img_num in zip(row, images):
           if num == img_num:
             screen.blit(pygame.image.load(images[img_num]), (x, y))
-          else:
-            continue
           x += 5
         y += 5
     else:
